7331ad71908623ee94c12469203d965d866ee706
Sebastian Bauersfeld <sebastian@semmle.com>
2022-08-02 14:58:43 +0700

    update

---
+++ integration-tests/iterate-cli-versions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+
+lang="$1"
+n="$2"
+
+current="v$(gh tailor actions-cli-version)"
+topn="$(gh codeql list-versions | head -n "$n"; echo "$current")"
+topn="$(printf "$topn" | sort -u)"
+
+
+for v in $(printf "$topn"); do
+  echo "Testing with version ${v}..."
+  gh codeql set-version "$v"
+  ./roundtrip.sh "$lang"
+done
---
+++ integration-tests/roundtrip.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -eu
+
+lang="$1"
+project="project"
+
+rm -rf "$project"
+
+# vanilla test
+gh tailor \
+  init \
+  -l "$1" \
+  -b "codeql/${lang}-queries" \
+  -n "zbazztian/gh-tailor-integration-test-${lang}" \
+  "$project"
+
+pushd "$project"
+
+# creation
+./create
+
+# tests
+#./integration-test
+#./test || echo "fails because there are not .expected files"
+#gh codeql test accept tests
+#./test
+
+# uploading
+./publish
+
+# The following autoversion should fail. If it
+# doesn't it is a bug and will fail the script.
+( \
+  gh \
+  tailor autoversion \
+  --mode new-on-collision \
+  --fail pack \
+  && \
+  exit -1
+) || true
