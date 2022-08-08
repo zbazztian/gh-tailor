c014370d828be6c8ae42570431ed54d6a442a108
Sebastian Bauersfeld <>
2022-08-08 10:51:48 +0700

    update

---
+++ templates/go/tests/test_1/test.go
@@ -0,0 +1,17 @@
+package main
+
+//import (
+//  "fmt"
+//  "net/http"
+//)
+
+func main() { }
+
+//func handler() {
+//  http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+//    r.ParseForm()
+//    //BAD
+//    fmt.Fprintf(w, "%q Hello", r.Form.Get("username"))
+//  })
+//  http.ListenAndServe(":80", nil)
+//}
---
+++ templates/go/tests/test_2/test.go
@@ -0,0 +1,14 @@
+package main
+
+//import (
+//  "io/ioutil"
+//  "net/http"
+//)
+
+func main() { }
+
+//func handler(w http.ResponseWriter, r *http.Request) {
+//  tainted_path := r.URL.Query()["path"][0]
+//  data, _ := ioutil.ReadFile(tainted_path)
+//  w.Write(data)
+//}
