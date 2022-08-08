43c8297b763bafc0c004137e470e30bd7b5be35d
Sebastian Bauersfeld <>
2022-08-08 12:15:02 +0700

    update

--- templates/java/tests/test_2/Test.java
+++ templates/java/tests/test_2/Test.java
@@ -1,34 +1,17 @@
-// Semmle test case for CWE-22: Improper Limitation of a Pathname to a Restricted Directory ('Path Traversal')
-// http://cwe.mitre.org/data/definitions/22.html
 import java.io.*;
 import java.net.InetAddress;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.FileSystems;
-
 
 class Test {
 	void doGet1(InetAddress address)
 		throws IOException {
-			String temp = address.getHostName();
-			File file;
-			Path path;
-			
-			// BAD: construct a file path with user input
-			file = new File(temp);
-			
-			// BAD: construct a path with user input
-			path = Paths.get(temp);
-					
-			// BAD: construct a path with user input
-			path = FileSystems.getDefault().getPath(temp);
-
-			// BAD: insufficient check
-			if (temp.startsWith("/some_safe_dir/")) {
-				file = new File(temp);
-			}
-
-			// BAD: construct a path with local user input
-			path = FileSystems.getDefault().getPath(System.getenv("PATH"));
+//			String temp = address.getHostName();
+//			
+//			// BAD: construct a file path with user input
+//			File file = new File(temp);
+//
+//			// MAYBE BAD: construct a path with local user input
+//			Path path = FileSystems.getDefault().getPath(System.getenv("PATH"));
 	}
 }
